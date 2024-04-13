ede09ee456f2eedf5c76ceb07be9ac1848c1c1da
kotoori <kotoori0815@gmail.com>
2024-04-13 23:40:39 +0900

    新Day13　変数、関数

--- New_Day13/kotoori_js-practice/js/script.js
+++ New_Day13/kotoori_js-practice/js/script.js
@@ -1,1 +1,42 @@
-alert("Hello, World!");
+let firstName = '太郎';
+let lastName = '山田';
+// console.log(firstName + " " + lastName + "さん");
+
+console.log(`${firstName} ${lastName}さん`);
+
+function say(firstName, lastName) {
+	console.log(`${firstName} ${lastName}さん、こんにちは`);
+}
+say('太郎', '山田');
+say('花子', '鈴木');
+say('ジョン', 'スミス');
+
+//円の面積を求める計算
+let radius = 10;
+const pai = 3.14;
+
+let area = radius * radius * pai;
+console.log(area);
+
+function calcCircleArea(radius) {
+	const pai = 3.14;
+
+	let area = radius * radius * pai;
+	return area;
+}
+console.log(calcCircleArea(10));
+console.log(calcCircleArea(100));
+
+
+function showMessage(message = 'No message') {
+	console.log(message);
+}
+showMessage('おはようございます');
+showMessage('こんにちは');
+showMessage(123);
+showMessage(123 * 10);
+
+function double(num) {
+	return num * 2;
+}
+console.log( double(2));
