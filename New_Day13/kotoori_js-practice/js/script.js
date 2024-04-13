1fbc92146578111bee6212c8a39bfa0fd32b9f3f
kotoori <kotoori0815@gmail.com>
2024-04-14 00:20:07 +0900

    新Day13　配列

--- New_Day13/kotoori_js-practice/js/script.js
+++ New_Day13/kotoori_js-practice/js/script.js
@@ -1,21 +1,22 @@
-if(true){
-	console.log("true");
-}
-if(false){
-	console.log("false");
-}
+const scores = [89, 56, 78];
+// console.log(scores);
+// console.log(scores.length);
 
-switch(score){
-	case 1:
-		console.log("1");
-		break;
-	case 2:
-		console.log("2");
-		break;
-	case 10:
-		console.log("10");
-		break;
-	default:
-		console.log("default");
-		break;
-}
+// scores[0] = 10;
+// console.log(scores[0]);
+// console.log(scores[1]);
+// console.log(scores[2]);
+
+scores.push(100);
+scores.push("Hello");
+scores.push(true);
+console.log(scores);
+
+scores.pop();
+console.log(scores);
+
+scores.shift();
+console.log(scores);
+
+scores.unshift(100);
+console.log(scores);
