a2e934c5823025873ff7ce17eab6b43ab6789a4d
kotoori <kotoori0815@gmail.com>
2024-04-14 10:01:30 +0900

    新Day13　繰り返し

--- New_Day13/kotoori_js-practice/js/script.js
+++ New_Day13/kotoori_js-practice/js/script.js
@@ -1,30 +1,16 @@
-const scores = {
-	math: 87,
-	english: 78,
-	science: 68
-}
+const prefectures = ["北海道", "青森県", "岩手県", "宮城県", "秋田県", "山形県", "福島県", "茨城県", "栃木県", "群馬県", "埼玉県", "千葉県", "東京都", "神奈川県", "新潟県", "富山県", "石川県", "福井県", "山梨県", "長野県", "岐阜県", "静岡県", "愛知県", "三重県", "滋賀県", "京都府", "大阪府", "兵庫県", "奈良県", "和歌山県", "鳥取県", "島根県", "岡山県", "広島県", "山口県", "徳島県", "香川県", "愛媛県", "高知県", "福岡県", "佐賀県", "長崎県", "熊本県", "大分県", "宮崎県", "鹿児島県", "沖縄県"];
 
-console.log(scores.math);
-console.log(scores['math']);
+//ofを使って配列の中身を一つずつ取り出す
+// for (let prefecture of prefectures){
+// 	console.log(prefecture);
+// }
 
-scores.math = 90;
-console.log(scores.math);
+//カウント
+// for (let i = 0; i < prefectures.length; i++){
+// 	console.log(prefectures[i]);
+// }
 
-const allScores = [
-	{math: 87, english: 56, science: 48},
-	{math: 45, english: 76, science: 89},
-	{math: 67, english: 88, science: 78}
-];
-
-console.log(allScores);
-
-const scores2 = {
-	math: 87,
-	english: 78,
-	science: 68,
-	messageEnglish: function(){
-		console.log(`英語の点数は${this.english}点です`);
-	}
-}
-
-scores2.messageEnglish();
+//forEachを使って配列の中身を一つずつ取り出す
+prefectures.forEach(function(prefecture){
+	console.log(prefecture);
+});
