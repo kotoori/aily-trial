0e34c871acf9e6ce9b6b6c6413676794a16ecc4c
kotoori <kotoori0815@gmail.com>
2024-04-15 12:33:20 +0900

    新Day16　DOMを操作する①　HTML要素を追加する

--- New_Day16/elementary-javascript/js/script.js
+++ New_Day16/elementary-javascript/js/script.js
@@ -1,46 +1,13 @@
-//id名で取得
-console.log(document.querySelector("#js-title").outerHTML);
-console.log(document.getElementById("js-title").outerHTML);
+//HTML要素のテキストを変更する
+document.querySelector("#js-title").innerText = "Daily Trial";
 
-//class名で取得
-const elements = document.querySelectorAll(".text");
-console.log(elements);
-for(let element of elements){
-	console.log(element.outerHTML);
-}
+//HTMLタグを追加する
+const button = document.createElement("a");
+button.innerText = "ログイン";
+button.setAttribute("class", "btn");
+button.setAttribute("href", "https://tokyofreelance.jp/");
 
-const elements2 = document.getElementsByClassName("text");
-console.log(elements2);
-for(let element of elements2){
-	console.log(element.outerHTML);
-}
+document.querySelector("#js-btn-wrap").appendChild(button);
 
-//タグ名で取得
-console.log(document.querySelector("h1").outerHTML);
-const elements3 = document.getElementsByTagName("h1");
-console.log(elements3);
-for(let element of elements3){
-	console.log(element.outerHTML);
-}
 
-//属性で取得
-console.log(document.querySelector('[href="https://www.daily-trial.com/"]').outerHTML);
-
-//実験
-// const array = [1, 2, 3, 4, 5];
-// const object = { name: "taro", age: 20 };
-// console.log(array);
-// console.log(object);
-// console.log(document.querySelectorAll("#js-title"));
-
-//メモ
-//document全体ではなくjs-section内から探して取得したい場合、どう書く？
-//↓
-  //まず、js-section要素を取得
-const section = document.querySelector("#js-section");
-console.log(section);
-  //js-section要素内の.textクラスをquerySelectorAllで取得
-const elements4 = section.querySelectorAll(".text");
-for(let element of elements4){
-	console.log(element.outerHTML);
-}
+console.log(button.outerHTML);
