0377afd9e2a1dc474a7c1d46693b089b971ee6e6
kotoori <kotoori0815@gmail.com>
2024-04-15 13:03:49 +0900

    新Day16　DOMを操作する②　クリックイベントを発生させる

--- New_Day16/elementary-javascript/js/script.js
+++ New_Day16/elementary-javascript/js/script.js
@@ -1,13 +1,2 @@
-//HTML要素のテキストを変更する
-document.querySelector("#js-title").innerText = "Daily Trial";
-
-//HTMLタグを追加する
-const button = document.createElement("a");
-button.innerText = "ログイン";
-button.setAttribute("class", "btn");
-button.setAttribute("href", "https://tokyofreelance.jp/");
-
-document.querySelector("#js-btn-wrap").appendChild(button);
-
-
-console.log(button.outerHTML);
+//クリックイベントをJSで発生させる
+document.querySelector("#js-register").click();
