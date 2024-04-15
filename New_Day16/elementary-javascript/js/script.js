f5e6dafa2a0a102a45bed07affe865b87e1f68f6
kotoori <kotoori0815@gmail.com>
2024-04-15 13:22:42 +0900

    新Day16　DOMを操作する③　イベント監視

--- New_Day16/elementary-javascript/js/script.js
+++ New_Day16/elementary-javascript/js/script.js
@@ -1,2 +1,9 @@
-//クリックイベントをJSで発生させる
-document.querySelector("#js-register").click();
+//読み込みが完了したらアラートを表示する
+// window.onload = function(){
+// 	alert("読み込みが完了しました！");
+// }
+
+//ボタンがクリックされたらアラートを表示する
+document.querySelector("#js-btn").addEventListener("click", function(){
+	alert("クリックされました！");
+});
